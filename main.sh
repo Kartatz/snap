9117542141315681292cc40ae1e58d69d593a163
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-09-05 04:29:57 -0300

    Update to LLVM LLD 21.1.0

---
+++ main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -r binutils_tarball='/tmp/binutils.tar.xz'
+declare -r binutils_directory='/tmp/binutils'
+
+git \
+	clone \
+	--depth '1' \
+	'https://sourceware.org/git/binutils-gdb.git' \
+	"${binutils_directory}"
+
+cd "${binutils_directory}"
+
+./src-release.sh 'binutils'
+
+tar \
+	--directory='/tmp' \
+	--extract \
+	--file="$(echo "${PWD}/binutils-"*'.tar')"
+
+rm --force --recursive "${binutils_directory}"
+
+mv '/tmp/binutils-'* "${binutils_directory}"
+
+tar \
+	--directory="$(dirname "${binutils_directory}")" \
+	--create \
+	--file=- \
+	'binutils' | \
+		xz \
+			--threads='0' \
+			--compress \
+			-9 > "${binutils_tarball}"
